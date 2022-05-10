b16f72bf172ade272018b64d54bdf61d76bc2466
Chen Yue Ying <tranvietanh190196@gmail.com>
2022-05-10 14:15:51 +0700

    update bash file sh

--- relayer/relay-run.sh
+++ relayer/relay-run.sh
@@ -2,7 +2,7 @@
 test=`ps aux | grep /home/ubuntu/pos-bridge/relayer | grep RelayApp.js | grep -v grep -c`
 if [ $test -eq 0 ]; then
    sudo rm -rf nohup.out
-   sudo npm install && sudo npm start & >> nohup.out
+   sudo npm install && sudo npm start >> nohup.out &
    echo "[INFO] Service is starting"
    exit
 else
