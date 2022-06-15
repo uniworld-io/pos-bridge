0f06fc251cdec7369af2a843148c4acda38cce16
Chen Yue Ying <tranvietanh190196@gmail.com>
2022-06-15 22:34:35 +0700

    - edit depositExec and withdrawExec

--- relayer/src/service/impl/UniPosBridgeService.ts
+++ relayer/src/service/impl/UniPosBridgeService.ts
@@ -1,10 +1,8 @@
-import {UniDepositContract, UniMapToken, UniWithdrawContract} from "../../entity/UniPosBridgeContract";
-import {UniSetupPosBridge} from "../../entity/UniPosBridgeContract";
+import {UniDepositContract, UniSetupPosBridge, UniWithdrawContract} from "../../entity/UniPosBridgeContract";
 import {poolConnector} from "../../config/PoolConnector";
 import {CHAIN} from "../../config/ConfigEnv";
 import {GroupVerification} from "../../entity/GroupVerification";
 import {PosBridgeService} from "../PosBridgeService";
-import {Urc20Burn} from "../../entity/Urc20";
 
 const unichain = poolConnector.uniChainConnector;
 const logger = require('../../common/Logger')
@@ -17,56 +15,42 @@ export class UniPosBridgeService implements PosBridgeService{
         return this.createTransaction(CHAIN.UNI.TEST.paths.setup, privateKey, setup);
     }
 
-    public async mapToken(mapToken: UniMapToken): Promise<any> {
-        const privateKey = CHAIN.UNI.TEST.admin.privateKey;
-        mapToken.owner_address = unichain.address.toHex(unichain.address.fromPrivateKey(privateKey));
-        return this.createTransaction(CHAIN.UNI.TEST.paths.mapToken, privateKey, mapToken);
-
-    }
-
-    public async unMapToken(mapToken: UniMapToken): Promise<any> {
-        const privateKey = CHAIN.UNI.TEST.admin.privateKey;
-        mapToken.owner_address = unichain.address.toHex(unichain.address.fromPrivateKey(privateKey));
-        return this.createTransaction(CHAIN.UNI.TEST.paths.unmapToken, privateKey, mapToken);
-    }
 
     public async deposit(deposit: UniDepositContract): Promise<any> {
         const privateKey = CHAIN.UNI.TEST.account2.privateKey;
         deposit.owner_address = unichain.address.toHex(unichain.address.fromPrivateKey(privateKey));
-        return this.createTransaction(CHAIN.UNI.TEST.paths.deposit, privateKey, deposit);
+
+        const tx = await unichain.transactionBuilder.posBridgeDeposit(
+            deposit.owner_address,
+            deposit.root_token,
+            deposit.child_chainid,
+            deposit.receive_address,
+            deposit.data);
+        const signedTx = await unichain.unx.signTransaction(tx, privateKey, 0)
+        const result = await unichain.unx.sendRawTransaction(signedTx);
+        console.log(result);
     }
 
     public async withdraw(withdraw: UniWithdrawContract): Promise<any> {
         const privateKey = CHAIN.UNI.TEST.account2.privateKey;
-        withdraw.owner_address = unichain.address.toHex(unichain.address.fromPrivateKey(privateKey));
-        return this.createTransaction(CHAIN.UNI.TEST.paths.withdraw, privateKey, withdraw);
+        const owner_address = unichain.address.toHex(unichain.address.fromPrivateKey(privateKey));
+        const tx = await unichain.transactionBuilder.posBridgeWithdraw(owner_address, withdraw.child_token, withdraw.receive_address, withdraw.data);
+        const signedTx = await unichain.unx.signTransaction(tx, privateKey, 0)
+        return await unichain.unx.sendRawTransaction(signedTx);
     }
 
     public async depositExec(verification: GroupVerification): Promise<any> {
         const privateKey = unichain.defaultPrivateKey;
-        const depositExec = {
-            owner_address: unichain.defaultAddress.hex,
-            message: verification.message,
-            signatures: verification.signatures
-        }
-        return this.createTransaction(CHAIN.UNI.DEPOSIT_EXEC_PATH, privateKey, depositExec);
+        const tx = await unichain.transactionBuilder.posBridgeDepositExec(unichain.defaultAddress.hex, verification.signatures, verification.message);
+        const signedTx = await unichain.unx.signTransaction(tx, privateKey, 0)
+        return await unichain.unx.sendRawTransaction(signedTx);
     }
 
     public async withdrawExec(verification: GroupVerification): Promise<any> {
         const privateKey = unichain.defaultPrivateKey;
-        const withdrawExec = {
-            owner_address: unichain.defaultAddress.hex,
-            message: verification.message,
-            signatures: verification.signatures
-        }
-        return this.createTransaction(CHAIN.UNI.WITHDRAW_EXEC_PATH, privateKey, withdrawExec);
-    }
-
-
-    public async urc20Burn(burner: Urc20Burn): Promise<any> {
-        const privateKey = CHAIN.UNI.TEST.admin.privateKey;
-        burner.owner_address = unichain.address.toHex(unichain.address.fromPrivateKey(privateKey));
-        return this.createTransaction("/wallet/urc20burn", privateKey, burner);
+        const tx = await unichain.transactionBuilder.posBridgeWithdraw(unichain.defaultAddress.hex, verification.signatures, verification.message);
+        const signedTx = await unichain.unx.signTransaction(tx, privateKey, 0)
+        return await unichain.unx.sendRawTransaction(signedTx);
     }
 
 
