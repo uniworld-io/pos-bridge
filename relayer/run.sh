1e434641c295a7824f8a29c8eebbb25cff734014
Chen Yue Ying <tranvietanh190196@gmail.com>
2022-06-15 11:01:42 +0700

    - script remove logs

--- relayer/run.sh
+++ relayer/run.sh
@@ -2,6 +2,7 @@
 test=`ps aux | grep /home/ubuntu/pos-bridge/relayer | grep RelayApp.js | grep -v grep -c`
 if [ $test -eq 0 ]; then
    sudo rm -rf nohup.out
+   sudo rm -rf logs/
    sudo npm install
    npm start >> nohup.out &
    echo "[INFO] Service is starting"
